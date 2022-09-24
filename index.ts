cb5b9c4260e04b3225421f4a9fa67fb6641c7dc1
mahi1292-dsa <114277113+mahi1292-dsa@users.noreply.github.com>
2022-09-24 11:14:40 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
-let list = [3, 4, 45, 6, 6, 7, 9];
-// searching for an element is present in the array or not
+var list = [3, 4, 6, 45, 6, 6, 7, 9];
+// list all the occurences of an element in array with index
+let occurences = [];
 function brute_force(list, query) {
   for (let i = 0; i < list.length; i++) {
     if (list[i] == query) {
-      return [i, list[i]];
+      occurences.push({ index: i, value: list[i] });
     }
   }
+  return occurences;
 }
-
 let res = brute_force(list, 6);
 console.log(res);
